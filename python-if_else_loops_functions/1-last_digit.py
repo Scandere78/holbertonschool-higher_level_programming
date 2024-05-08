a325cecca392b740652cd8b1090e4c1d4c1d798a
Scandere78 <scanderetejpro@gmail.com>
2024-05-08 16:00:47 +0200

    1. The last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ else:
 if last_di > 5:
     print(f"Last digit of {number} is {last_di} and is greater than 5")
 elif last_di == 0:
-    print(f"Last digit of {number} is {last_di} and is zero")
+    print(f"Last digit of {number} is {last_di} and is 0")
 elif last_di < 6 and last_di != 0:
     print(f"Last digit of {number} is {last_di} and is less than 6 and not 0")
