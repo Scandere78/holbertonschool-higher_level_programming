af52d67c18b31fa0c0d2881262ce896aee7a37be
Scandere78 <scanderetejpro@gmail.com>
2024-05-09 16:43:54 +0200

    add files

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 
-if len(argv) == 1:
-    print("0 argument.")
-    print(".")
-else:
-    print("{} argument{}:".format(len(argv) - 1, 's' if len(argv) > 2 else ''))
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 argument.")
+        print(".")
+    else:
+        print("{} argument{}:".format(len(argv) - 1, 's' if len(argv) > 2 else ''))
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
+
