f23cf521bd42eb4506f2c9c63495174b17328762
Scandere78 <scanderetejpro@gmail.com>
2024-05-09 16:28:48 +0200

    add files

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from 1-calculation import add, sub, mul, div
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
