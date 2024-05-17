2b6cedd3327f86750f8bc534db46db0b45e501d2
Scandere78 <scanderetejpro@gmail.com>
2024-05-17 16:04:55 +0200

    add the files

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    """ A class that defines a square by its size
+    """
+    Class that defines properties of square by: (based on 3-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1size).
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """ Method that returns the square are of the object
+        """Calculates the aera of square.
+
+        Returns: the current square aera.
         """
-        return (self.__size ** 2)
+        return self.__size ** 2
 
     @property
     def size(self):
-        """ Method to returns the size value
+        """Returns: the size of a square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
+        """Property setter for size.
+
+            Args:
+                value (int): size of a square (1 side).
+
+            Raises:
+                TypeError: size must be an integer
+                ValueError: size must be >= 0
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
