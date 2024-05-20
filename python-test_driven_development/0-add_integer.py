c32ed689a31ed331959b23aa753ecf38b92c673b
Scandere78 <scanderetejpro@gmail.com>
2024-05-20 17:16:43 +0200

    add the file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 function that adds two numbers
 
 
-"""I
+"""
 
 def add_integer(a, b=98):
     """ Function adds two integer and/or float numbers
@@ -24,7 +24,7 @@ def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("a must be an integer")
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
     return (a + b)
