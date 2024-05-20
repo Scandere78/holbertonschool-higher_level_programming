62daffa405dbc6b02c879b666c8a20b7e4522d8f
Scandere78 <scanderetejpro@gmail.com>
2024-05-21 01:12:20 +0200

    la 2 j'ai git de quelqun sa ma fait male au crane le projet

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
+"""
+
+This module is composed by a function that divides the numbers of a matrix
+
+"""
+
 
 def matrix_divided(matrix, div):
+    """ Function that divides the integer/float numbers of a matrix
+
+    Args:
+        matrix: list of a lists of integers/floats
+        div: number which divides the matrix
+
+    Returns:
+        A new matrix with the result of the division
+
+    Raises:
+        TypeError: If the elements of the matrix aren't lists
+                   If the elemetns of the lists aren't integers/floats
+                   If div is not an integer/float number
+                   If the lists of the matrix don't have the same size
+
+        ZeroDivisionError: If div is zero
+
+
+    """
 
     if not type(div) in (int, float):
-        raise TypeError("div msut be a number")
+        raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    msg_type = "matrix must be a matrix (lis of lists) of integers/floats"
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
 
     if not matrix or not isinstance(matrix, list):
         raise TypeError(msg_type)
@@ -17,7 +42,6 @@ def matrix_divided(matrix, div):
     msg_size = "Each row of the matrix must have the same size"
 
     for elems in matrix:
-
         if not elems or not isinstance(elems, list):
             raise TypeError(msg_type)
 
