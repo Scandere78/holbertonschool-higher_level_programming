be26dc09028eec3d0ac27e99cae22c6614676ea6
Scandere78 <scanderetejpro@gmail.com>
2024-05-23 16:28:23 +0200

    add the files

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+
+function that returns the list of available attributes and methods of an object
+
+"""
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
