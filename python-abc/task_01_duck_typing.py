0a387b3547ea0351814378cd86d27b7eb6d7177b
Scandere78 <scanderetejpro@gmail.com>
2024-05-25 18:08:05 +0200

    add the files

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+ Class file shapes multiples
+
+"""
+
+
+from abc import ABC, abstractmethod
+import math
+""" Define the abstract class Shape"""
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+""" Define the Circle class"""
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+""" Define the Rectangle class"""
+
+
+class Rectangle(Shape):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
