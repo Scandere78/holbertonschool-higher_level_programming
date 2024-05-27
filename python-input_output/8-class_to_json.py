addd7f660d41bc2ad7fdb70f894554992bb84457
Scandere78 <scanderetejpro@gmail.com>
2024-05-27 17:32:39 +0200

    add the files

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    file for function class_to_json
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns dictionary description suitable for JSON serialization of object
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary containing serializable attributes of the object.
+    """
+
+    json_dict = {}
+    for attr, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[attr] = value
+    return json_dict
