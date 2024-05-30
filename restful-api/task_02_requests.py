2c71ab1d7e5011443a51daea8911f04d39785f06
Scandere78 <scanderetejpro@gmail.com>
2024-05-30 14:08:14 +0200

    add the files

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,27 +4,27 @@ import csv
 
 
 def fetch_and_print_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status Code: {response.status_code}")
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f'Status Code: {response.status_code}')
 
     if response.status_code == 200:
-        posts = response.json()
-        for post in posts:
-            print(post['title'])
-    else:
-        print("Failed to fetch posts")
+        data = response.json()
+        for dd in data:
+            print(dd['title'])
 
 
 def fetch_and_save_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
     if response.status_code == 200:
-        posts = response.json()
-        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-            writer.writeheader()
-            writer.writerows(posts_data)
+        with open('posts.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            fields = ['id', 'title', 'body']
 
-        print("Posts have been saved to posts.csv")
-    else:
-        print("Failed to fetch posts")
+            writer.writerow(fields)
+            data = response.json()
+            for dd in data:
+                writer.writerow([
+                    dd['id'],
+                    dd['title'],
+                    dd['body']
+                ])
