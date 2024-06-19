9632e2e0e91b044ddb284e00ed654ae37c3d8bcc
Scandere78 <scanderetejpro@gmail.com>
2024-06-19 13:54:54 +0200

    add the files SQL

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
 -- script Read-only user
 --create db
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;i
+--create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- attribute privilege user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
