a5a3c1766407e04e238384906907ef079653dbbf
Scandere78 <scanderetejpro@gmail.com>
2024-09-03 15:30:52 +0200

    add the task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env
+#!/usr/bin/python3
 def uppercase(str):
-    for i in range (len(str)):
+    for i in range(len(str)):
         s = str[i]
-        uppercase = ord(s)
-        if uppercase >= 97 and unicode <= 122:
-            s = chr(uppercase - 32)
+        unicode = ord(s)
+        if unicode >= 97 and unicode <= 122:
+            s = chr(unicode - 32)
         print("{:s}".format(s), end="")
     print("")
