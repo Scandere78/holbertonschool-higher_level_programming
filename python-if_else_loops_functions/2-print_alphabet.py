bd4e48979d6df216cc5334811fa6df52c99772dc
ScandereT78 <scanderetejpro@gmail.com>
2024-09-03 11:53:57 +0100

    add the task 2 and 3

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == ord('e') or alpha_letters == ord('q'):
-        continue
-    print("{:c}".format(alpha_letters), end="")
+for letter in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(letter)), end="")
