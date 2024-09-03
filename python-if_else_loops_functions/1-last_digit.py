9f2ef2ad6b27562b70ff7d9553a7aa67c8f301c2
ScandereT78 <scanderetejpro@gmail.com>
2024-09-03 11:37:03 +0100

    add the task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
+
+# pour obtenir le dernier numéro
+if number >= 0:
     last_di = number % 10
 else:
     last_di = number % -10
@@ -13,5 +15,5 @@ elif last_di == 0:
     result = "0"
 else:
     result = "less than 6 and not 0"
-print(f"Last digit of {number} is {last_di} and is {result}")
 
+print(f"Last digit of {number} is {last_di} and is {result}")
