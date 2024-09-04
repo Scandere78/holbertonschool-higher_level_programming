a961f07092ece747ca7261a70657b76849bc5f83
Scandere78 <scanderetejpro@gmail.com>
2024-09-04 16:52:48 +0200

    add the task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,13 @@
 from sys import argv
 
 if __name__ == "__main__":
-    args = argv[1:]  # No need to use sys.argv, just argv
-    num_args = len(args)
-
-    if num_args == 0:
-        print("Number of argument(s): 0.")
-    elif num_args == 1:
-        print("1 argument:")
-    else:
-        print(f"{num_args} arguments:")
-
-    for i, arg in enumerate(args, start=1):
-        print(f"{i}: {arg}")
+    argc = len(argv) - 1
+    print("{:d} {}{}".format(
+        argc,
+        "argument" if argc == 1 else "arguments",
+        "." if argc < 1 else ":"
+    ))
+    if argc >= 1:
+        for i in range(1, len(argv)):
+            arg = argv[i]
+            print("{}: {}".format(i, arg))
