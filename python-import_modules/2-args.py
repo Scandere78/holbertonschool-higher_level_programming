0c5f1959570e91e79c7aa9373d3383a3f9451998
ScandereT78 <scanderetejpro@gmail.com>
2024-09-04 15:39:58 +0100

    add the task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    num_args = len(argv)
+
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+    elif num_args == 1:
+        print(f"Number of argument(s): 1 argument:")
+    else:
+        print(f"Number of argument(s): {num_args} arguments:")
+
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
+
+
