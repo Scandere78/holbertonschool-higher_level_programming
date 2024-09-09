419305769423be045c7c9d6dd46dd5b021e98021
ScandereT78 <scanderetejpro@gmail.com>
2024-09-09 13:59:19 +0100

    add the task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for letters in my_string:
-        if letters != "c" or letters != "C":
+        if letters != "c" and letters != "C":
             new_string += letters
     return new_string
