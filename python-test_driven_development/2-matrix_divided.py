fc079b489b402fc37807c0e83ce2a8ef12c37650
ScandereT78 <scanderetejpro@gmail.com>
2024-09-13 11:11:59 +0100

    add the task

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,6 +6,22 @@ a matrix by a given divisor.
 
 
 def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by a given divisor.
+
+    Args:
+        matrix (list of lists of int/float): The matrix to be divided.
+        div (int/float): The divisor.
+
+    Returns:
+        list of lists of float: A new matrix with all elements divided by div.
+
+    Raises:
+        TypeError: If matrix is not a list of lists of integers/floats.
+        TypeError: If each row of the matrix is not of the same size.
+        TypeError: If div is not a number (integer or float).
+        ZeroDivisionError: If div is equal to 0.
+    """
     if not isinstance(matrix, list) or \
             not all(isinstance(row, list) for row in matrix):
         raise TypeError(
