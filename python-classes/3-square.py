1be008cfffe64320e000b8649c05e1a822eb5797
ScandereT78 <scanderetejpro@gmail.com>
2024-09-16 12:04:55 +0100

    add the task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Defines a square """
+"""Defines a square."""
 
 
-class Square():
-    """initialise valeur size"""
-
+class Square:
+    """Defines a square."""
     def __init__(self, size=0):
+        """Initialize data.
+
+        Args:
+            size (int, optional): Size of the square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
+
     def area(self):
-        """return size ** 2"""
-        return self.__size ** 2
+        """Get the current square area."""
+        return self.__size * self.__size
 
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Defines a square """
+"""Defines a square."""
 
 
-class Square():
-    """initialise valeur size"""
-
+class Square:
+    """Defines a square."""
     def __init__(self, size=0):
-        self.__size = size
+        """Initialize data.
+
+        Args:
+            size (int, optional): Size of the square.
+        """
+        self.size = size
+
+    def area(self):
+        """Get the current square area."""
+        return self.__size * self.__size
 
     @property
-    def area(self):
-        """return size """
+    def size(self):
+        """Get the size of square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter of the size field."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        return self.__size ** 2
