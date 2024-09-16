1e6811374803ce2264ff97de33a7d285ad62ae65
ScandereT78 <scanderetejpro@gmail.com>
2024-09-16 12:06:37 +0100

    add the task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Defines a square """
+"""Defines a square."""
 
 
-class Square():
-    """initialise valeur size"""
-    __size = None
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initialize data.
 
-    def __init__(self, size):
+        Args:
+            size (int, optional): Size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
