5639c756acf077fd87d3c743b85bc7a31e29f049
ScandereT78 <scanderetejpro@gmail.com>
2024-09-19 15:36:31 +0100

    add the task

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,15 @@ Shebang to specify the Python 3 interpreter.
 
 
 def is_same_class(obj, a_class):
+    """
+    Check if an object is exactly an instance of a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if obj is an instance of a_class, False otherwise.
+    """
+
     return type(obj) is a_class
