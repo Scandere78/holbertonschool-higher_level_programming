5772c10ce831e63fb5f9e297cab140c47a38b2c2
ScandereT78 <scanderetejpro@gmail.com>
2024-09-20 16:59:10 +0100

    add the task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,7 +6,8 @@ Shebang to specify the Python 3 interpreter.
 
 class BaseGeometry:
     """
-    A class BaseGeometry with a public instance method 'area'.
+    A class BaseGeometry with a public instance method 'area' and
+    'integer_validator' to validate values.
     """
     def area(self):
         """
@@ -15,7 +16,12 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Validates that 'value' is an integer and greater than 0.
+        Raises TypeError if 'value' is not an integer.
+        Raises ValueError if 'value' is <= 0.
+        """
         if not isinstance(value, int):
-            raise TypeError (f"{name} must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-             raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
