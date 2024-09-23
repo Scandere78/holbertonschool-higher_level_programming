49b98f35fb59b65c06321f40fc2f4edb65ce524b
ScandereT78 <scanderetejpro@gmail.com>
2024-09-23 15:57:52 +0100

    add the task

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        self.count += 1
+        return next(self.iterator)
+
+    def get_count(self):
+        return self.count
