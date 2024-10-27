3ac0da68e720e73c8efbbd2b4fe8bdff1c98069a
ScandereT78 <scanderetejpro@gmail.com>
2024-10-27 22:25:43 +0100

    fix the task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     password = sys.argv[2]
     database = sys.argv[3]
     
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY id ASC") 
     query_rows = cur.fetchall()
