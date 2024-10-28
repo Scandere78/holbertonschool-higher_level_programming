cb7804f9b3fff6430151bec565b42e6fee224e7a
Scandere78 <scanderetejpro@gmail.com>
2024-10-28 14:13:17 +0100

    add the task 0-1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' \
                 ORDER BY states.id ASC")
     query_rows = cur.fetchall()
 
