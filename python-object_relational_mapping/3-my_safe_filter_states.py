b2134f5b6ce0d3714de90bf2a570ff73371b1702
Scandere78 <scanderetejpro@gmail.com>
2024-10-28 20:56:36 +0100

    add task 2

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists states where name matches arg
+Sys.Args: username, password, db, state
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        if rows[1] == sys.argv[4]:
+            print(row)
+
+    cur.close()
+    db.close()
