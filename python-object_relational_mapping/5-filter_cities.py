078adac25e62bf6156a02edc3641f26c1586742e
Scandere78 <scanderetejpro@gmail.com>
2024-10-28 21:30:03 +0100

    Votre message de commit ici

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,56 @@
 #!/usr/bin/python3
+"""
+    Script that lists all states from the database.
+"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cur = db.cursor()
-    query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
+
+def connectDb(user, password, db):
     """
-    cur.execute(query, (sys.argv[4],))
+        Get connection with the database.
+        Args:
+            user (str): Username of the user.
+            password (str): Password of the user.
+            db (str): Database to retrieve.
+        Return:
+            Connection database.
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db,
+        charset="utf8"
+    )
+    return conn
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    arg = sys.argv[4]
+
+    conn = connectDb(user, password, db)
+    cur = conn.cursor()
+
+    query = f"""
+        SELECT cities.name
+        FROM cities
+        JOIN
+            states ON states.id = cities.state_id
+        WHERE
+            states.name = %s
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query, (arg,))
+
     query_rows = cur.fetchall()
-
-    if query_rows:
-        print(", ".join(row[0] for row in query_rows))
-    else:
-        print("")
-
+    results = []
+    for row in query_rows:
+        results.append(row[0])
+    print(", ".join(results))
     cur.close()
-    db.close()
+    conn.close()
