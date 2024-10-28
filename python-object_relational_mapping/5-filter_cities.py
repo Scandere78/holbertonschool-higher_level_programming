44861bd22ce43611db4be33404f2da649b2234de
Scandere78 <scanderetejpro@gmail.com>
2024-10-28 21:21:24 +0100

    add task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name \
                 FROM cities \
-                JOIN states \
+                INNER JOIN states \
                 ON cities.state_id = states.id \
                 WHERE states.name = '{}' \
                 ORDER BY cities.id ASC".format(sys.argv[4]))
