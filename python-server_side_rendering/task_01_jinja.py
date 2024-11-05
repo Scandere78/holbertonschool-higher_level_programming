844989b2dedb7174fee5cb3b836cefbe9946fb1e
ScandereT78 <scanderetejpro@gmail.com>
2024-11-05 18:06:06 +0100

    fix the task 01

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
-
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -14,5 +14,6 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
